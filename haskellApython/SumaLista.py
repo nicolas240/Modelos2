fd409bcc8abb7c5437965cafbd7f429b71e5174f
JJaraSas <49918831+JJaraSas@users.noreply.github.com>
2019-05-19 14:28:52 -0500

    Delete SumaLista.py

--- haskellApython/SumaLista.py
+++
@@ -1,9 +0,0 @@
-def sumalista(listaNumeros):
-    if len(listaNumeros) == 0:
-        return 0
-    elif len(listaNumeros) == 1:
-        return listaNumeros[0]
-    else:
-        return listaNumeros[0] + sumalista(listaNumeros[1:])
-
-print(sumalista([]))
