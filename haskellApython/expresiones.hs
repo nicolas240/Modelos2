d78bb969d0c7265609c0c7d5bb4f48a3231e4860
nicolas240 <nicolasmadero24@gmail.com>
2019-05-18 21:53:42 -0500

    funciones haskell en python

--- haskellApython/expresiones.hs
+++ haskellApython/expresiones.hs
@@ -1,3 +1,29 @@
+--pag42
 sumar:: [Int]->Int
 sumar [] = 0
 sumar (x:xs) = x +sumar(xs)
+
+--pag43
+invertir::Ord a=>[a]->[a]
+invertir [] = []
+invertir (x:xs) = (invertir xs )++[x]
+
+--pag 44
+igualLista:: Eq a => [a]->[a]->Bool
+igualLista l1 l2 = l1 == l2
+
+--pag 45
+listaOrdenada:: Ord a=>[a]->Bool
+listaOrdenada []=True
+listaOrdenada [_]=True
+listaOrdenada (x:y:xs) = (x<=y) && listaOrdenada(y:xs) 
+
+--pag 46
+mostrarUbicacion:: Ord a=>[a]->Int->a
+mostrarUbicacion l n = l!!n
+
+--pag 47
+mayor :: [Int]->Int
+mayor (x:xs)
+   | x > mayor(xs) = x
+   | otherwise = mayor(xs)
--- haskellApython/expresiones.py
+++ haskellApython/expresiones.py
@@ -1,9 +1,18 @@
 #!bin/python2.7
 
-
+#pag 42
 def sumar(x):
 	result=0
 	for a in x: result=result+a
 	print result
+	return result
 
-sumar([5,4,7,8])
+#pag 44
+def igualar(x,y):
+	if y!=x:
+		print "falso"
+		return False
+
+#pag 46
+def ubicacion(x,n):
+	return x[n]
