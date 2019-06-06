1713656d4ac3d083af817a5a33c711e6ce08fcaa
JJaraSas <49918831+JJaraSas@users.noreply.github.com>
2019-06-05 19:23:09 -0500

    Update arboles.py

--- python/laberinto/arboles.py
+++ python/laberinto/arboles.py
@@ -11,30 +11,50 @@ def buscar(arbol, valor):
     return buscarHijos(arbol.hijos, valor)
 
 def buscarHijos(hijos, valor):
-	if hijos == []: return False
-	return buscar(hijos[0], valor) or buscarHijos(hijos[1:], valor)
+    if hijos == []: return False
+    return buscar(hijos[0], valor) or buscarHijos(hijos[1:], valor)
 
-def a_lista(arbol):
-    if arbol == []:
-        return []
-    if len(arbol.hijos)==1:
-        return a_lista( [arbol.valor] +  a_lista(arbol.hijos[0]))
-    return a_lista( [arbol.valor])+ a_lista(arbol.hijos[1:])
-
-def insertar(arbol, valor):
+def arbol_lista(arbol):
+    #print (arbol.valor)
     if arbol == None:
-        return Nodo(valor)
-    if arbol.valor < valor:
-        return Nodo(arbol.valor, arbol.izq, insertar(arbol.der, valor))
-    return Nodo(arbol.valor, insertar(arbol.izq, valor), arbol.der)
+        #return []
+        return []
+    print ('arbol', arbol.valor)
+    return [arbol.valor] + hijos_lista(arbol.hijos)
 
-def crearArbol(arbol, lista):
-    if lista == []:
+def hijos_lista(hijos):
+    if hijos == []:
+        return []
+        #return None
+    if len(hijos) == 1:
+        return arbol_lista(hijos[0])
+    return arbol_lista(hijos[0]) + hijos_lista(hijos[1:])
+
+#Albol ya tiene raiz
+def insertar(arbol, padre ,valor):
+    print ('No es Padre: ', arbol.valor)
+    if arbol == None:
+        print ('Vacio')
         return arbol
-    if len(lista) > 0:
-        return crearArbol(insertar(arbol, lista[0]), lista[1:])
+    if arbol.valor == padre:
+        #print ('Es Padre: ', arbol.valor)
+        arbol.hijos.append(valor)
+        print (arbol.hijos)
+        return Nodo(arbol.valor, arbol.hijos)
+    #print arbol_lista(Nodo(arbol.valor, arbol.hijos))
+    return Nodo(arbol.valor, insertarEnHijos(arbol.hijos, padre, valor))
 
-arbol = Nodo(25, [Nodo(10, []),Nodo(40, [])])
-print buscar(arbol, 12)
-a_lista(arbol)
+def insertarEnHijos(hijos, padre, valor):
+    #print ('hijos')
+    if len(hijos) < 1:
+        print ('hijos Vacio')
+        return []
+    return [insertar(hijos[0], padre, valor)] + [insertarEnHijos(hijos[1:], padre, valor)]
 
+
+arbol = Nodo(25, [Nodo(10, [Nodo(12, [])]), Nodo(40, [Nodo(36, [])]), Nodo(50, [Nodo(27, [])]) ] )
+
+#print (buscar(arbol, 28))
+print (arbol_lista(arbol))
+arbol = insertar(arbol, 27, 8)
+print (arbol_lista(arbol))
